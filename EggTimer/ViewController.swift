7ed40e647070a470be3b1f2d87aa96766169b3a4
davidcode10 <davidedson2030@outlook.com>
2022-05-19 14:30:54 +0200

    countdown implemented

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    var counter = 0
+    
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print(counter)
+            titleChange.text = "Preparing eggs"
+            counter -= 1
+        } else if counter == 0 {
+            titleChange.text = "Done!"
+        }
+    }
+    
+    var timer = Timer()
+    
+    let eggTime = ["Soft": 3, "Medium": 4, "Hard": 7]
+    
+    @IBOutlet weak var titleChange: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+        let hardness = sender.currentTitle!
+        
+        counter = eggTime[hardness]!
+        
+    }
+    
 }
