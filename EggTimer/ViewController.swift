09e34d42431334f190ace6ee43b7eb069c1461d2
davidcode10 <davidedson2030@outlook.com>
2022-05-19 23:33:00 +0200

    implemented audio player when countdown finished

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
     
     @objc func updateCounter() {
         if secondsPassed < totalTime {
@@ -17,6 +20,7 @@ class ViewController: UIViewController {
         } else {
             timer.invalidate()
             titleChange.text = "Done!"
+            playSound(soundName: "alarm_sound")
         }
     }
     
@@ -24,7 +28,7 @@ class ViewController: UIViewController {
     var secondsPassed = 0
     var totalTime = 0
     
-    let eggTime = ["Soft": 300, "Medium": 420, "Hard": 720]
+    let eggTime = ["Soft": 3, "Medium": 420, "Hard": 720]
     
     @IBOutlet weak var titleChange: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!    
@@ -41,4 +45,9 @@ class ViewController: UIViewController {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
